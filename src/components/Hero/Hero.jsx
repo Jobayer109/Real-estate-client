41573a06d7b4903c044fc3108254e4fe11a4c189
user.email <jobayer.ahmed109@gmail.com>
2023-07-19 14:01:51 +0600

    Hero title and search bar done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { HiLocationMarker } from "react-icons/hi";
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings flexCenter innerWidth hero-container">
-        <div className="hero-left">
-          <p>Hero left</p>
+        {/* Left side */}
+        <div className="hero-left flexColStart">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover <br /> Most Suitable <br /> Property.
+            </h1>
+          </div>
+          <div className="hero-description flexColStart">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you.</span>
+          </div>
+          <div className="search-bar flexCenter">
+            <HiLocationMarker color="var(--blue)" size={30} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
         </div>
+
+        {/* Right side */}
         <div className="hero-right flexCenter">
           <div className="image-container">
             <img src="./hero-image.png" alt="" />
