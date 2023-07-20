8ed4f04f27f3dad10b930a6b509bfac34f03b879
user.email <jobayer.ahmed109@gmail.com>
2023-07-20 23:52:49 +0600

    Contact section half done

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { MdCall } from "react-icons/md";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings flexCenter innerWidth c-container">
+        {/* Left side */}
+        <div className="c-left ">
+          <div className="flexColStart">
+            <span className="orangeText">Contact us</span>
+            <span className="primaryText">Easy to contact us</span>
+            <span className="secondaryText">
+              We always ready to help by providing the best services for you. We believe a <br />{" "}
+              good vibe to live can make your life better.
+            </span>
+            <div className="flexColStart contactModes">
+              {/* First row */}
+              <div className="flexColStart row">
+                <div className="flexColCenter mode">
+                  <div className="flexStart">
+                    <div className="flexCenter icon">
+                      <MdCall size={25} />
+                    </div>
+                    <div className="flexColStart detail">
+                      <span>Call</span>
+                      <span>+880 1734 053 116</span>
+                    </div>
+                  </div>
+                  <div className="flexCenter button">Call Now</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Right side */}
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
